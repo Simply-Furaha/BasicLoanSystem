6c983630196694065fb286ccb3843d8784da5333
FURAHA MAXWELL EGESA <146840417+Simply-Furaha@users.noreply.github.com>
2024-02-15 17:50:38 +0300

    This is a basic Java ciode for loan system

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,59 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Your Official Names: ");
+        String names = scanner.nextLine();
+        System.out.println("Enter Your Age: ");
+        int age = scanner.nextInt();
+        System.out.println("Enter Your Monthly income: ");
+        double income = scanner.nextDouble();
+        if (age >= 18 && age <= 34 && income > 10000) {
+            System.out.println("Hello " + names + "You Qualify for the followwing loans:/n 1. Youth,/n 2. Hustler,/n 3. Biashara");
+            System.out.println("Select the loan you want to apply for(1,2,3): ");
+            int loan = scanner.nextInt();
+            if (loan == 1) {
+                System.out.println("You have been approved for the Youth loan");
+            } else if (loan == 2) {
+                System.out.println("You have been approved for the Hustler loan");
+            } else if (loan == 3) {
+                System.out.println("You have been approved for the Biashara loan");
+            } else {
+                System.out.println("Invalid loan selection");
+            }
         }
+        if (age > 35 && age <= 44 && income > 100000) {
+            System.out.println("Hello " + names + "You are Qualify for the followwing loans:/n 1. Asset Finance,/n 2. Personal Loan,/n 3. Business Loan");
+            System.out.println("Select the loan you want to apply for(1,2,3): ");
+            int loan = scanner.nextInt();
+            if (loan == 1) {
+                System.out.println("You have been approved for the Asset Finance loan");
+            } else if (loan == 2) {
+                System.out.println("You have been approved for the Personal loan");
+            } else if (loan == 3) {
+                System.out.println("You have been approved for the Business loan");
+            } else {
+                System.out.println("Invalid loan selection");
+            }
+        }
+        if (age > 45 && age < 90 && income > 500000) {
+            System.out.println("Hello " + names + "You are Qualify for the followwing loans:/n 1. Jijenge,/n 2. Biashara");
+            System.out.println("Select the loan you want to apply for(1,2): ");
+            int loan = scanner.nextInt();
+            if (loan == 1) {
+                System.out.println("You have been approved for the Asset finance loan");
+            } else if (loan == 2) {
+                System.out.println("You have been approved for the Mega Loan loan");
+            } else {
+                System.out.println("Invalid loan selection");
+            }
+        }
+        else {
+            System.out.println("You do not qualify for any loan");
+        }
+
     }
 }
